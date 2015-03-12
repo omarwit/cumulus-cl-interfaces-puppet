cc1e38354341cc2f3fbe29c1446f0e445b32369d
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2015-03-12 16:08:48 -0400

    begin to create framework for ifupdown2 helper class.

--- lib/cumulus/ifupdown2.rb
+++ lib/cumulus/ifupdown2.rb
@@ -1,5 +1,30 @@
-module Cumulus
-  module Ifupdown2
+class Ifupdown2Config
+  attr_accessor :confighash
+  def initialize(resource)
+    @resource = resource
+  end
+
+  def update_addr_method
+  end
+
+  def update_address
+  end
+
+  def update_attr(suffix=nil)
+  end
+
+  # updates alias name in confighash
+  def update_alias_name
+  end
+
+  # updates vrr config in config hash
+  def update_vrr
+  end
+
+  ## comparision
+  def <=>(another_config)
+  end
+
+  def write_config
   end
 end
-
--- lib/puppet/provider/cumulus_interface/ruby.rb
+++ lib/puppet/provider/cumulus_interface/ruby.rb
@@ -1,5 +1,32 @@
 require 'cumulus/ifupdown2'
 Puppet::Type.type(:cumulus_interface).provide :ruby do
-  confine operatingsystem: [:cumulus_linux]
-  include Cumulus::Ifupdown2
+  #confine operatingsystem: [:cumulus_linux]
+
+  def build_desired_config
+    desired_config = Ifupdown2Config.new(resource)
+    desired_config.update_addr_method
+    desired_config.update_address
+    %w(vids pvid).each do |attr|
+      desired_config.update_attr(attr, 'bridge')
+    end
+    desired_config.update_alias_name
+    desired_config.update_vrr
+    # attributes with no suffix like bond-, or bridge-
+    %w(mstpctl-portnetwork mstpctl-bpduguard clagd_enable clagd_priority
+    clagd_args clagd_peer_ip).each do |attr|
+      desired_config.update_attr(attr)
+    end
+    # copy to instance variable
+    @desired_config = desired_config
+  end
+
+  def config_changed?
+    @current_config = Ifupdown2Config.new(resource)
+    build_desired_config
+    (@current_config == @desired_config) ? :outofsync : :insync
+  end
+
+  def update_config
+    @desired_config.write_config
+  end
 end
