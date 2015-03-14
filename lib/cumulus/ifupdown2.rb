59d9c2f353f7aaca1090552e125b92ae3fa5411c
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2015-03-14 19:31:47 -0400

    refactor getting ip address code

--- lib/cumulus/ifupdown2.rb
+++ lib/cumulus/ifupdown2.rb
@@ -38,7 +38,7 @@ class Ifupdown2Config
   def hash_to_if
     intf = ''
     cmd = "/sbin/ifquery -i - -t json #{@resource[:name]}"
-    IO.popen(cmd, mode="r+") do |ifquery|
+    IO.popen(cmd,mode = 'r+') do |ifquery|
       ifquery.write([@confighash].to_json)
       ifquery.close_write
       intf = ifquery.read
@@ -53,62 +53,54 @@ class Ifupdown2Config
 
   def update_addr_method
     unless @resource[:addr_method].nil?
-      Puppet.info "updating address method"
-      @confighash["addr_method"] = @resource[:addr_method].to_s
-      @confighash["addr_family"] = "inet"
+      Puppet.info "updating address method #{@resource[:name]}"
+      @confighash['addr_method'] = @resource[:addr_method].to_s
+      @confighash['addr_family'] = 'inet'
     end
 
+  end
+
+  def build_address(addr_type)
+    return nil.to_s if @resource[addr_type.to_sym].nil?
+    Puppet.debug "updating #{addr_type} info #{@resource[:name]}"
+    @resource[addr_type.to_sym].join(' ')
   end
 
   def update_address
-    addresslist = ''
-    unless @resource[:ipv4].nil?
-      Puppet.debug "updating ipv4 info #{@resource[:name]}"
-      addresslist +=  @resource[:ipv4].join(' ')
-      addresslist += ' '
-    end
-    unless @resource[:ipv6].nil?
-      Puppet.debug "updating ipv6 info #{@resource[:name]}"
-      addresslist += @resource[:ipv6].join(' ')
-    end
-    unless addresslist.empty?
-      @confighash["config"]["address"] = addresslist
-    end
+    addresslist = build_address('ipv4')
+    addresslist += ' ' + build_address('ipv6')
+    return if addresslist.strip.empty?
+    @confighash['config']['address'] = addresslist
   end
 
-  def update_attr(attr, suffix=nil)
+  def update_attr(attr, suffix = nil)
     resource_value = @resource[attr.to_sym]
     ifupdown_value = ''
-    if resource_value.nil?
-      return
-    end
+    return if resource_value.nil?
     if resource_value == true
       ifupdown_value = 'yes'
     elsif resource_value ==  false
       ifupdown_value = 'no'
-    elsif resource_value.kind_of?(Array)
+    elsif resource_value.is_a?(Array)
       ifupdown_value = resource_value.join(' ')
     else
       ifupdown_value = resource_value.to_s
     end
     # ifquery uses dash not underscore to define attributes
-    attr.sub! '_' , '-'
+    attr.sub! '_', '-'
     configattr = (suffix.nil?) ? attr : "#{suffix}-#{attr}"
     @confighash['config'][configattr] = ifupdown_value
   end
 
   # updates alias name in confighash
   def update_alias_name
-    unless @resource[:alias_name].nil?
-      Puppet.debug "updating alias #{@resource[:name]}"
-      @confighash["config"]["alias"] = @resource[:alias_name]
-    end
+    return if @resource[:alias_name].nil?
+    Puppet.debug "updating alias #{@resource[:name]}"
+    @confighash['config']['alias'] = @resource[:alias_name]
   end
 
   def update_speed
-    if @resource[:speed].nil?
-      return
-    end
+    return if @resource[:speed].nil?
     Puppet.debug "configuring speed #{@resource[:name]}"
     @confighash['config']['link-speed'] = @resource[:speed].to_s
     @confighash['config']['link-duplex'] = 'full'
@@ -116,11 +108,10 @@ class Ifupdown2Config
 
   # updates vrr config in config hash
   def update_vrr
-    unless @resource[:virtual_ip].nil?
-      vrrstring = @resource[:virtual_mac] + ' ' + @resource[:virtual_ip]
-      @confighash["config"]["address-virtual"] = vrrstring
-      Puppet.debug "updating vrr config #{vrrstring}"
-    end
+    return if @resource[:virtual_ip].nil?
+    vrrstring = @resource[:virtual_mac] + ' ' + @resource[:virtual_ip]
+    @confighash['config']['address-virtual'] = vrrstring
+    Puppet.debug "updating vrr config #{vrrstring}"
   end
 
   def update_members(attrname, ifupdown_attr)
@@ -137,8 +128,8 @@ class Ifupdown2Config
   end
 
   ## comparision
-  def ==(another_config)
-    @confighash == another_config.confighash
+  def ==(other)
+    @confighash == other.confighash
   end
 
   # convert hash to text using ifquery
@@ -146,10 +137,10 @@ class Ifupdown2Config
   def write_config
     Puppet.info "write config for #{@resource[:name]}"
     intf = hash_to_if
-    filepath = @resource[:location] + "/" +  @resource[:name]
+    filepath = @resource[:location] + '/' +  @resource[:name]
     Puppet.debug "file location: #{filepath}"
     begin
-      ifacefile = File.open(filepath, "w")
+      ifacefile = File.open(filepath, 'w')
       ifacefile.write(intf)
     ensure
       ifacefile.close
--- spec/unit/type/cumulus_interface_spec.rb
+++ spec/unit/type/cumulus_interface_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-require 'pry'
+require 'pry-debugger'
 cl_iface = Puppet::Type.type(:cumulus_interface)
 
 describe cl_iface do
