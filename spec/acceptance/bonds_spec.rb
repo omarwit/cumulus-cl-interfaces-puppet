2f785ba1572133dfdb599ed8793b3dcb92d77f03
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2015-08-24 14:19:47 +0100

    Add tests for LACP bypass attributes

--- spec/acceptance/bonds_spec.rb
+++ spec/acceptance/bonds_spec.rb
@@ -12,26 +12,29 @@ describe 'interfaces' do
 
         # Bond, over-ride all defaults
         cumulus_bond { 'bond1':
-          slaves              => ['swp5-6'],
-          ipv4                => ['10.0.0.1/24', '192.168.1.0/16'],
-          ipv6                => ['2001:db8:abcd::/48'],
-          alias_name          => 'bond number 1',
-          #addr_method        => 'static',
-          min_links           => 2,
-          mode                => 'balance-alb',
-          miimon              => 99,
-          xmit_hash_policy    => 'layer2',
-          lacp_rate           => 1,
-          mtu                 => 9000,
+          slaves                 => ['swp5-6'],
+          ipv4                   => ['10.0.0.1/24', '192.168.1.0/16'],
+          ipv6                   => ['2001:db8:abcd::/48'],
+          alias_name             => 'bond number 1',
+          #addr_method           => 'static',
+          min_links              => 2,
+          mode                   => 'balance-alb',
+          miimon                 => 99,
+          xmit_hash_policy       => 'layer2',
+          lacp_rate              => 1,
+          mtu                    => 9000,
           # ifquery doesn't seem to like clagd related parameters on an interface?
-          # clag_id           => 1,
-          vids                => ['1-4094'],
-          pvid                => 1,
-          virtual_mac         => '11:22:33:44:55:FF',
-          virtual_ip          => '192.168.20.1',
-          mstpctl_portnetwork => true,
-          mstpctl_bpduguard   => true,
-          notify              => Service['networking'],
+          # clag_id              => 1,
+          vids                   => ['1-4094'],
+          pvid                   => 1,
+          virtual_mac            => '11:22:33:44:55:FF',
+          virtual_ip             => '192.168.20.1',
+          mstpctl_portnetwork    => true,
+          mstpctl_bpduguard      => true,
+          lacp_bypass_allow      => 1,
+          lacp_bypass_period     => 30,
+          lacp_bypass_all_active => 1,
+          notify                 => Service['networking'],
         }
 
         file { '/etc/network/interfaces':
@@ -83,6 +86,9 @@ describe 'interfaces' do
       its(:content) { should match(/address-virtual 11:22:33:44:55:FF 192.168.20.1/) }
       its(:content) { should match(/mstpctl-portnetwork yes/) }
       its(:content) { should match(/mstpctl-bpduguard yes/) }
+      its(:content) { should match(/bond-lacp-bypass-period 30/) }
+      its(:content) { should match(/bond-lacp-bypass-all-active 1/) }
+      its(:content) { should match(/bond-lacp-bypass-allow 1/) }
     end
   end
 end
